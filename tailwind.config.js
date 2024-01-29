dc60c99a9dd58f5cec8c9843bf2c145696c1572c
shivank mishra <shivankmishra710@gmail.com>
2024-01-29 23:24:44 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100&display=swap');"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
